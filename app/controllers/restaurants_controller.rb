9d3336fcda8618ec84a9cd0c66f294b5db88890d
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 18:39:39 +0300

    Commented on the index & added a show method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,17 @@ class RestaurantsController < ApplicationController
 
     # GET /restaurants
     def index 
+      # Retrieve all the restaurants
         restaurants = Restaurant.all
+      # Render the restaurants objs to json format
         render json: restaurants, status: :ok
+    end
+
+    # GET /restarants/:id
+    def show
+       # Retrieve a single restaurant using its ID
+        restaurant = Restaurant.find(params[:id])
+       # Render the restaurant's obj to a json format 
+        render json: restaurant, status: :ok
     end
 end
