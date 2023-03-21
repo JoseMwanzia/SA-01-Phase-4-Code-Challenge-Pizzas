54386e2547d9ebd692ba4b76e23afcb1b22d6e0f
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 22:17:36 +0300

    Created a create method

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -7,4 +7,13 @@ class PizzasController < ApplicationController
        # Render the pizzas obj to a json obj
         render json: pizzas, status: :ok
     end
+
+    # POST /restaurant_pizzas
+    def create
+       # Create a pizza 
+        pizza = Pizza.create(pizzas_params)
+       # Render the pizzas obj to a json obj
+        render json: pizza, status: :created
+    end
+
 end
