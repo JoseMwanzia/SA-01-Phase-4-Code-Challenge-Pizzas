4b7ef4111bd169813775db699e3e7dfa93363819
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 22:44:09 +0300

    Added a create method and a private method

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantPizzasController < ApplicationController
+    # POST /restaurant_pizzas
+    def create
+        # Create a pizza 
+        pizza = Pizza.create(pizzas_params)
+        # Render the pizzas obj to a json obj
+        render json: pizza, status: :created
+    end
+
+    private 
+
+    def pizzas_params
+        params.permit(:name, :ingredients)
+    end
 end
