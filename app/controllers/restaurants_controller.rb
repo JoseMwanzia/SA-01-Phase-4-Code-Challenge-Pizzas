6c746ce35086de489c63f83e2880f1a2ffabc478
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 18:27:18 +0300

    Added an index method in the restaurant controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,8 @@
 class RestaurantsController < ApplicationController
+
+    # GET /restaurants
+    def index 
+        restaurants = Restaurant.all
+        render json: restaurants, status: :ok
+    end
 end
