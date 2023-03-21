10e84f8ca3b304ddc0e1c63dfd7d951bc141ac08
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 22:32:50 +0300

    Updated pizzas controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -8,12 +8,4 @@ class PizzasController < ApplicationController
         render json: pizzas, status: :ok
     end
 
-    # POST /restaurant_pizzas
-    def create
-       # Create a pizza 
-        pizza = Pizza.create(pizzas_params)
-       # Render the pizzas obj to a json obj
-        render json: pizza, status: :created
-    end
-
 end
