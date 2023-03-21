5dd4bf01ffe8acc9d758d8ca65d7c2fd61c45bc1
joseph mwanzia <makamwanzia@gmail.com>
2023-03-21 22:01:20 +0300

    Added an index method to pizzas controllers

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,10 @@
 class PizzasController < ApplicationController
+
+    # GET /pizzas
+    def index
+       # Get all the pizzas from tha Pizza instance 
+        pizzas = Pizza.all
+       # Render the pizzas obj to a json obj
+        render json: pizzas, status: :ok
+    end
 end
