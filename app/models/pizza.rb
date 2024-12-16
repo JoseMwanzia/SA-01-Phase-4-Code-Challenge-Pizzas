f3cb494eb900827eb2a4f4e6d7e3512f511c9df3
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-16 16:48:07 +0300

    fix: fix issues getting pizzas

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,3 +1,3 @@
 class Pizza < ApplicationRecord
-    has_many :restaurants, through: :restaurant_pizza
+    has_many :restaurants, through: :restaurant_pizzas
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,3 @@
 class Restaurant < ApplicationRecord
-    has_many :pizzas, through: :restaurant_pizza, dependent: :destroy
+    has_many :pizzas, through: :restaurant_pizzas, dependent: :destroy
 end
